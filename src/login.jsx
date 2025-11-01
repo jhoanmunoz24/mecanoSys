774caf9cb4f0ec4f860b87e66757bf5a49fabb73
jhoanmunoz24 <pirrijhoan@gmail.com>
2025-10-31 23:22:01 -0500

    Version Base

---
+++ src/login.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+
+export default function LoginForm() {
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center">
+        <div className="max-w-md w-full p-6 bg-white rounded-lg shadow-lg">
+          <div className="flex justify-center mb-10">
+            <h2 className="text-2xl text-black font-medium" >Mecano<span className='text-cyan-400'>Sys</span></h2>
+            
+          </div>
+
+          <h1 className="text-2xl font-semibold text-center text-gray-500 mt-8 mb-6">
+            Iniciar sesión
+          </h1>
+
+          <form>
+            <div className="mb-6">
+              <label
+                htmlFor="email"
+                className="block mb-2 text-sm text-gray-600"
+              >
+                Correo electrónico
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-500"
+                required
+              />
+            </div>
+
+            <div className="mb-6">
+              <label
+                htmlFor="password"
+                className="block mb-2 text-sm text-gray-600"
+              >
+                Contraseña
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-cyan-500"
+                required
+              />
+              <a
+                href="#"
+                className="block text-right text-xs text-cyan-600 mt-2"
+              >
+                ¿Olvidaste tu contraseña?
+              </a>
+            </div>
+
+            <button
+              type="submit"
+              className=" w-32 bg-gradient-to-r from-cyan-400 to-cyan-600 text-white py-2 rounded-lg mx-auto block focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500 mt-4 mb-6  hover:from-cyan-300 hover:to-cyan-600"
+            >
+              Acceso
+            </button>
+          </form>
+
+          <div className="text-center">
+            <p className="text-sm">
+              ¿No tienes una cuenta?{" "}
+              <a href="#" className="text-cyan-600">
+                Regístrate ahora
+              </a>
+            </p>
+          </div>
+
+          <p className="text-xs text-gray-600 text-center mt-10">
+            &copy; 2025 MecanoSys
+          </p>
+        </div>
+      </div>
+      
+    </>
+  )
+}
